273e1d1f338a5e92d1e6ab340911bb98df157355
Carlos Bermeo <caenbera@gmail.com>
2026-01-17 21:57:48 +0000

    Try fixing this error: `Runtime Error: Error: Couldn't find next-intl co

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,6 @@
 import {getRequestConfig} from 'next-intl/server';
 import {notFound} from 'next/navigation';
-import {locales} from './i18n-config';
+import {locales} from '../i18n-config';
  
 export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
