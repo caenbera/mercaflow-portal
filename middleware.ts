0285ae7d06afba5023458fc7581f3c8cd318c04d
Carlos Bermeo <caenbera@gmail.com>
2026-01-17 21:46:15 +0000

    Try fixing this error: `Runtime Error: Error: Couldn't find next-intl co

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import createMiddleware from 'next-intl/middleware';
-import { locales, localePrefix, defaultLocale } from './src/i18n-config';
+import { locales, localePrefix, defaultLocale } from './i18n-config';
 
 export default createMiddleware({
   locales,
