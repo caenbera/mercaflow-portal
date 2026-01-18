253f479a876b9beba30d1a4b26861f1f978f4f63
Carlos Bermeo <caenbera@gmail.com>
2026-01-18 16:45:36 +0000

    tan bien que estabamos, y otra vez los problemas: "Error: The Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,2 @@
-import createMiddleware from 'next-intl/middleware';
-import { locales, defaultLocale, localePrefix } from '@/i18n-config';
-
-export default createMiddleware({
-  locales,
-  defaultLocale,
-  localePrefix,
-});
-
-export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
-};
+// The active middleware is located in /src/middleware.ts
+// This file is intentionally left empty to avoid conflicts.
