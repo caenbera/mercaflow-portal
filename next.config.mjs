ff2d9c3edd06eaba585a69dd2a75aa6505e33ef4
Carlos Bermeo <caenbera@gmail.com>
2026-02-07 16:13:04 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,45 @@
-// @ts-check
 
-/**
- * @type {import('next').NextConfig}
- */
+import createNextIntlPlugin from 'next-intl/plugin';
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'i.postimg.cc',
-            },
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-            },
-            {
-                protocol: 'https',
-                hostname: 'via.placeholder.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 's.groovevideo.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'ui-avatars.com',
-            },
-        ],
-    },
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 's.groovevideo.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ui-avatars.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
-const withNextIntl = require('next-intl/plugin')('./src/i18n.ts');
- 
-module.exports = withNextIntl(nextConfig);
+const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
+
+export default withNextIntl(nextConfig);
