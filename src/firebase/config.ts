2220eb3bcb3c5bbaeb8184629f1def68f3a032a8
Carlos Bermeo <caenbera@gmail.com>
2026-02-15 19:57:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "mercaflow-portal-9155343-a8010",
+  "appId": "1:510513313642:web:75116fe53e93c8daad9713",
+  "apiKey": "AIzaSyDE_kd7IsvYEtoa1Zj2TgzjZY_8xegeYRU",
+  "authDomain": "mercaflow-portal-9155343-a8010.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "510513313642"
+};
